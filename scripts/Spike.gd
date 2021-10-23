1485f745395131b157e6f7b2f8413e0858a50b65
Nader <nader.ma@live.com>
2021-10-23 18:34:32 +0200

    renamed the seed object

--- scripts/Spike.gd
+++ scripts/Spike.gd
@@ -1,6 +1,6 @@
 extends Area2D
 
 func _on_Spike_body_entered(body):
-	if body.name == "Player" or body.name == "Node2D":
+	if body.name == "Player" or body.name == "seed":
 		queue_free()
 		get_tree().reload_current_scene()
