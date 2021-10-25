3d478633ac09dee573d879b8ad401654f0a74a9f
Nader <nader.ma@live.com>
2021-10-25 21:47:38 +0200

    new level, minor features and fixes!

--- scripts/restart_input_object.gd
+++ scripts/restart_input_object.gd
@@ -4,3 +4,7 @@ func _process(delta):
 	if Input.is_action_just_pressed("restart_button"):
 		queue_free()
 		get_tree().reload_current_scene()
+		
+	if Input.is_action_just_pressed("ui_cancel"):
+		get_tree().quit()
+	
